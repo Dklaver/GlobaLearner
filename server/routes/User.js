c4d0bca8aa3e7181c64c65b13f794e704ea9d4f7
Dklaver <512957@student.fontys.nl>
2023-10-08 16:46:35 +0200

    ORM database

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+const { User } = require("../models")
+
 router.get("/", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    User.findAll().then((users)=> {
+        res.send(users);
+    }).catch((err)=> {
+        console.log(err);
+    });
+});
+
+router.get("/create", (req, res) => {
+    User.create({
+        name: "Daniel",
+        country: "Netherlands",
+        email: "Daniel@gmail.com",
+        password: "password"
+    }).catch((err) =>{
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.send("insert");
 })
 
-router.post("/", (req, res) => {
-
+router.get("/delete", (req,res) => {
+    User.destroy({ where: {id: 4} })
+    res.send("delete");
 })
 
 module.exports = router; 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 
-const userRoute = require('./routes/User')
-const chatRoute = require('./routes/Chat')
+const db = require("./models");
+
+const userRoute = require('./routes/User');
+const chatRoute = require('./routes/Chat');
 
 app.use('/users', userRoute);
 app.use('/chat', chatRoute);
 
-app.listen(5000, () => {
-    console.log(`Server is listening to 5000`);
+
+db.sequelize.sync().then((req) => {
+    app.listen(5000, () => {
+        console.log(`Server is listening to 5000`);
+    })
 })
