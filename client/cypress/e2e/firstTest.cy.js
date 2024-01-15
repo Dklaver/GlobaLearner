2cd83d9c57e0585d3ebdaeed8f986170f15b4826
Dklaver <512957@student.fontys.nl>
2024-01-15 17:06:32 +0100

    Update firstTest.cy.js

--- client/cypress/e2e/firstTest.cy.js
+++ client/cypress/e2e/firstTest.cy.js
@@ -1,81 +1,74 @@
 
+describe('End-to-End Registration', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
 
-describe('end to end', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-
-    cy.get('[data-testid="cypress-navtitle"]').should("exist").should('have.text', 'Globalearner');
-    cy.get('[data-testid="cypress-navchat"]').should("exist").should('have.text', 'Chat');
-    cy.get('[data-testid="cypress-navregister"]').should("exist").should('have.text', 'Register').click();
+  it('should register a user', () => {
+    cy.get('[data-testid="cypress-navtitle"]').should('exist').and('have.text', 'Globalearner');
+    cy.get('[data-testid="cypress-navchat"]').should('exist').and('have.text', 'Chat');
+    cy.get('[data-testid="cypress-navregister"]').should('exist').and('have.text', 'Register').click();
 
     // go to register
-    cy.get('[data-testid="cypress-RegisterSignup"]').should('exist').should('be.disabled');
+    cy.get('[data-testid="cypress-RegisterSignup"]').should('exist').and('be.disabled');
 
-    cy.get('[data-testid="cypress-registerUsername"]').should("exist").type('a');
-    cy.get('[data-testid="cypress-registerUsernameCheck"]').should("exist").should('have.text', ' 4 to 24 characters.Must begin with a letter.letters, numbers allowed')
+    cy.get('[data-testid="cypress-registerUsername"]').should('exist').type('a');
+    cy.get('[data-testid="cypress-registerUsernameCheck"]').should('exist').and('have.text', ' 4 to 24 characters.Must begin with a letter.letters, numbers allowed');
     cy.get('[data-testid="cypress-registerUsername"]').type('{backspace}1234');
 
-    cy.get('[data-testid="cypress-registerPassword"]').should("exist").type('TestPass1');
-    cy.get('[data-testid="cypress-registerMatch"]').should("exist").type('TestPass2');
-    cy.get('[data-testid="cypress-passwordMatch"]').should('have.text', ' Must be the same as the password above.')
+    cy.get('[data-testid="cypress-registerPassword"]').should('exist').type('TestPass1');
+    cy.get('[data-testid="cypress-registerMatch"]').should('exist').type('TestPass2');
+    cy.get('[data-testid="cypress-passwordMatch"]').should('have.text', ' Must be the same as the password above.');
     cy.get('[data-testid="cypress-registerMatch"]').type('{backspace}1');
 
     cy.get('[data-testid="cypress-RegisterSignup"]').should('not.be.disabled');
+  });
+});
 
-    // go to login
+describe('After Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login');
+  });
 
-    
-    
-  })
-})
-
-describe('after login', () => {
-  it ('passes', () => {
-    cy.visit('http://localhost:3000/login')
-
-    cy.get('[data-testid="cypress-LoginUsername"]').should("exist").type('cypress');
+  it('should perform actions after login', () => {
+    cy.get('[data-testid="cypress-LoginUsername"]').should('exist').type('cypress');
     cy.get('[data-testid="cypress-loginPassword"]').type('Cypress1');
-    cy.get('[data-testid="cypress-loginSignin"]').should("exist").click();
+    cy.get('[data-testid="cypress-loginSignin"]').should('exist').click();
 
     // back to home
     cy.wait(2000);
     
-    cy.get('[data-testid="cypress-navchat"]').should("exist").should('have.text', 'Chat').click();
+    cy.get('[data-testid="cypress-navchat"]').should('exist').and('have.text', 'Chat').click();
 
-    cy.wait(2000);
     // go to chat
+    cy.wait(2000);
+
     cy.window().then((win) => {
       const jwtToken = win.localStorage.getItem('jwt');
-
       expect(jwtToken).to.not.be.null;
-
-      cy.log('token: ', jwtToken)
-    }),
+      cy.log('token: ', jwtToken);
+    });
     
     cy.wait(3000);
 
-    cy.get('[data-testid="cypress-createChatButton"]').should('exist');
-    cy.get('[data-testid="cypress-createChatButton"]').click();
-
-    cy.get('[data-testid="cypress-createChatName"]').should("exist").type('CypressTest');
-    cy.get('[data-testid="cypress-createChatLanguage"]').should("exist").type('English');
+    cy.get('[data-testid="cypress-createChatButton"]').should('exist').click();
+    cy.get('[data-testid="cypress-createChatName"]').should('exist').type('CypressTest');
+    cy.get('[data-testid="cypress-createChatLanguage"]').should('exist').type('English');
 
     cy.wait(2000);
 
-    cy.get('[data-testid="cypress-createChat"]').should("exist").click();
+    cy.get('[data-testid="cypress-createChat"]').should('exist').click();
 
-    //go in chat/$id
-    cy.wait(2000);
+    // go in chat/$id
+    cy.wait(5000);
 
-    cy.get('[data-testid="cypress-messageInput"]').should("exist").type("this is a cypress test {enter}");
+    cy.get('[data-testid="cypress-messageInput"]').should('exist').type("this is a cypress test {enter}");
 
-    
     cy.reload();
 
-    cy.get('[data-testid="cypress-messageToChat"]').should("exist").click();
+    cy.get('[data-testid="cypress-messageToChat"]').should('exist').click();
 
-    //back to chat list page
-
-    cy.get('[data-testid="cypress-showUsersChats"]').should("exist").click();
-  })
-})
+    // back to chat list page
+    cy.get('[data-testid="cypress-showUsersChats"]').should('exist').click();
+  });
+});
