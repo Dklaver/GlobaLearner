3dc8a1ba6460e57fee4047a98e776c67ad069fe3
Dklaver <512957@student.fontys.nl>
2024-01-15 14:33:40 +0100

    Update Navbar.js

--- client/src/Pages/Shared/Navbar.js
+++ client/src/Pages/Shared/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import './Navbar.css';
 import { NavLink } from "react-router-dom";
 import axios from "../../axios";
